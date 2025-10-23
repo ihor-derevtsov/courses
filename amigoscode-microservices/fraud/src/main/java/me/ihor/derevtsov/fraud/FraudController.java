1fcd4cd59e89c9d20190f721074dbeedb7aca111
Ihor Derevtsov <ihor.derevtsov.dev@gmail.com>
2025-10-23 19:04:03 +0200

    feat(eureka-server): add load balancing

--- amigoscode-microservices/fraud/src/main/java/me/ihor/derevtsov/fraud/FraudController.java
+++ amigoscode-microservices/fraud/src/main/java/me/ihor/derevtsov/fraud/FraudController.java
@@ -2,7 +2,10 @@ package me.ihor.derevtsov.fraud;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
@@ -15,7 +18,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
-        log.info("Fraud check request for customer {}", customerId);
+        log.info("Fraud check request for customer ID {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
