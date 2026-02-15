2c86189962ea5dbc60722ce2cae2b4e92bd322ed
Ihor Derevtsov <ihorderevtsov@gmail.com>
2026-02-15 16:14:13 +0100

    feat(k8s): refactor microservices to deploy to k8s cluster

--- amigoscode-microservices/clients/src/main/java/me/ihor/derevtsov/clients/fraud/FraudClient.java
+++ amigoscode-microservices/clients/src/main/java/me/ihor/derevtsov/clients/fraud/FraudClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "fraud")
+@FeignClient(name = "fraud", url = "${clients.fraud.url}")
 public interface FraudClient {
     /**
      * Points to api/v1/fraud-check/{customerId} endpoint of FraudController magically
