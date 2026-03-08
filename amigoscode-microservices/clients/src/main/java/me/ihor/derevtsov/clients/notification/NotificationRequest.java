d9011a6d3aed35d973f479af04a8e44c760186d2
Ihor Derevtsov <ihorderevtsov@gmail.com>
2026-03-08 21:18:24 +0100

    chore: move amigoscode-microservices to root folder

--- amigoscode-microservices/clients/src/main/java/me/ihor/derevtsov/clients/notification/NotificationRequest.java
+++
@@ -1,8 +0,0 @@
-package me.ihor.derevtsov.clients.notification;
-
-public record NotificationRequest(
-        Integer toCustomerId,
-        String toCustomerEmail,
-        String message
-) {
-}
--- amigoscode-microservices/notification/src/main/java/me/ihor/derevtsov/notification/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package me.ihor.derevtsov.notification;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-}
